f236b527aa198191fa3ef8acd16ff29c5d39052e
josh <joshuaking34111573@gmail.com>
2023-03-16 10:45:02 -0700

    c commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include <sys/syscall.h>
+#include <unistd.h>
+
 /**
- * main - entry
- * Return: Always 1(success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, str, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
+
+
