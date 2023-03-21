66ca754eb0b07a511ec20e150df4732b7ad74b63
josh <joshuaking34111573@gmail.com>
2023-03-21 09:52:26 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 {
 	int a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 97; b < 122; b++)
+		for (b = 97; b <= 122; b++)
 		{
 			_putchar(b);
 		}
