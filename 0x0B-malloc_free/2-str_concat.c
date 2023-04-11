a8ef5e3f77623493761ec0ff6003c351219dbdc6
josh <joshuaking34111573@gmail.com>
2023-04-11 08:15:34 -0700

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,44 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * str_concat - returns a pointer to a newly allocated space.
- * @str: string.
- * @s1: storaged char
- * @s2: storaged char
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
  *
  * Return: pointer of an array of chars
  */
 char *str_concat(char *s1, char *s2)
 {
-        char *str;
-        unsigned int i, j, k, l;
+	char *ns;
+	unsigned int i, j, k, temp;
 
-        if (s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-                s2 = "";
+		s2 = "";
 
-        for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-	for (j = 0; str[j] != '\0'; j++);
+	ns = (char *)malloc(sizeof(char) * (i + j + 1));
 
-        str = (char *) malloc(sizeof(char) * (i + j + 1));
-
-        if (str == NULL)
+	if (ns == NULL)
 	{
-		free(str);
-                return (NULL);
+		free(ns);
+		return (NULL);
 	}
-	for (k = 0; k <= i; k++)
-                str[k] = s1[k];
 
-	for (l = 0; l <= j; l++)
-                str[l] = s2[l];
+	for (k = 0; k < i; k++)
+		ns[k] = s1[k];
 
-        return (str);
+	temp = j;
+	for (j = 0; j <= temp; k++, j++)
+		ns[k] = s2[j];
+
+	return (ns);
 }
 ~                                                                                                              
 ~                             
